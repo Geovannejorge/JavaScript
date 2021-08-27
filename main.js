d06b209c90f5826c6cb1addea8678430c75d6ba5
Geovannejorge <64450808+Geovannejorge@users.noreply.github.com>
2021-08-27 17:42:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+//Tratamento de strings
+/*
+var nome ="Micaela Macedo ";
+var idade ="30";
+alert("Seja bem Vindo "+ nome + " " + idade);
+*/
+
+//Nesse bloco de código usamos o replase para subtituir uma string ou texto
+var frase = "Trabalhando Strings no Java Script"
+console.log(frase.replace("Trabalhando","Executando"));
+//-----------------------------------------------------
+//Alterando todas as letras para Maiusculas
+console.log(frase.toLocaleUpperCase());
+//-------------------------------------------------------
+//Alterando todas as letras para Minusculas
+console.log(frase.toLowerCase());
+
+
